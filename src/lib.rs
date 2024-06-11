7b4b9195dba5a7415894de7db96851e77a1454f2
tranvanthanh99 <thanhvantran99@gmail.com>
2024-06-11 11:34:01 +0700

    feat: :sparkles: add AnchorDeserialize for jupiter instruction

--- src/lib.rs
+++ src/lib.rs
@@ -57,17 +57,17 @@ pub fn find_jupiter_open_orders(market: &Pubkey, authority: &Pubkey) -> Pubkey {
 pub mod jupiter_override {
     use anchor_lang::InstructionData;
     use anchor_lang::{prelude::*, Discriminator};
-    use jupiter_amm_interface::Swap as InterfaceSwap;
+    // use jupiter_amm_interface::Swap as InterfaceSwap;
 
-    #[derive(AnchorSerialize, Debug)]
+    #[derive(AnchorSerialize, AnchorDeserialize, Debug)]
     pub struct RoutePlanStep {
-        pub swap: InterfaceSwap,
+        pub swap: crate::Swap,
         pub percent: u8,
         pub input_index: u8,
         pub output_index: u8,
     }
 
-    #[derive(AnchorSerialize)]
+    #[derive(AnchorSerialize, AnchorDeserialize)]
     pub struct Route {
         pub route_plan: Vec<RoutePlanStep>,
         pub in_amount: u64,
@@ -81,7 +81,7 @@ pub mod jupiter_override {
 
     impl InstructionData for Route {}
 
-    #[derive(AnchorSerialize)]
+    #[derive(AnchorSerialize, AnchorDeserialize)]
     pub struct RouteWithTokenLedger {
         pub route_plan: Vec<RoutePlanStep>,
         pub quoted_out_amount: u64,
@@ -94,7 +94,7 @@ pub mod jupiter_override {
 
     impl InstructionData for RouteWithTokenLedger {}
 
-    #[derive(AnchorSerialize)]
+    #[derive(AnchorSerialize, AnchorDeserialize)]
     pub struct SharedAccountsRoute {
         pub id: u8,
         pub route_plan: Vec<RoutePlanStep>,
@@ -109,7 +109,7 @@ pub mod jupiter_override {
 
     impl InstructionData for SharedAccountsRoute {}
 
-    #[derive(AnchorSerialize)]
+    #[derive(AnchorSerialize, AnchorDeserialize)]
     pub struct SharedAccountsRouteWithTokenLedger {
         pub id: u8,
         pub route_plan: Vec<RoutePlanStep>,
@@ -123,4 +123,106 @@ pub mod jupiter_override {
     }
 
     impl InstructionData for SharedAccountsRouteWithTokenLedger {}
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone, Debug, PartialEq)]
+pub enum Side {
+    Bid,
+    Ask,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone, Debug, PartialEq)]
+pub enum Swap {
+    Saber,
+    SaberAddDecimalsDeposit,
+    SaberAddDecimalsWithdraw,
+    TokenSwap,
+    Sencha,
+    Step,
+    Cropper,
+    Raydium,
+    Crema {
+        a_to_b: bool,
+    },
+    Lifinity,
+    Mercurial,
+    Cykura,
+    Serum {
+        side: Side,
+    },
+    MarinadeDeposit,
+    MarinadeUnstake,
+    Aldrin {
+        side: Side,
+    },
+    AldrinV2 {
+        side: Side,
+    },
+    Whirlpool {
+        a_to_b: bool,
+    },
+    Invariant {
+        x_to_y: bool,
+    },
+    Meteora,
+    GooseFX,
+    DeltaFi {
+        stable: bool,
+    },
+    Balansol,
+    MarcoPolo {
+        x_to_y: bool,
+    },
+    Dradex {
+        side: Side,
+    },
+    LifinityV2,
+    RaydiumClmm,
+    Openbook {
+        side: Side,
+    },
+    Phoenix {
+        side: Side,
+    },
+    Symmetry {
+        from_token_id: u64,
+        to_token_id: u64,
+    },
+    TokenSwapV2,
+    HeliumTreasuryManagementRedeemV0,
+    StakeDexStakeWrappedSol,
+    StakeDexSwapViaStake {
+        bridge_stake_seed: u32,
+    },
+    GooseFXV2,
+    Perps,
+    PerpsAddLiquidity,
+    PerpsRemoveLiquidity,
+    MeteoraDlmm,
+    OpenBookV2 {
+        side: Side,
+    },
+    RaydiumClmmV2,
+    StakeDexPrefundWithdrawStakeAndDepositStake {
+        bridge_stake_seed: u32,
+    },
+    Clone {
+        pool_index: u8,
+        quantity_is_input: bool,
+        quantity_is_collateral: bool,
+    },
+    SanctumS {
+        src_lst_value_calc_accs: u8,
+        dst_lst_value_calc_accs: u8,
+        src_lst_index: u32,
+        dst_lst_index: u32,
+    },
+    SanctumSAddLiquidity {
+        lst_value_calc_accs: u8,
+        lst_index: u32,
+    },
+    SanctumSRemoveLiquidity {
+        lst_value_calc_accs: u8,
+        lst_index: u32,
+    },
 }
